452497bb5c840d34717176fa361211cf31263f47
Follpvosten <wolfi@karpador.xyz>
2019-12-15 20:03:50 +0100

    Fix for default import

--- src/pouchdb_sys.rs
+++ src/pouchdb_sys.rs
@@ -3,23 +3,24 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "pouchdb")]
 extern "C" {
+    #[wasm_bindgen(js_name = default)]
     pub type PouchDB;
 
-    #[wasm_bindgen(constructor)]
+    #[wasm_bindgen(constructor, js_class = default)]
     pub fn new(name_or_opts: JsValue) -> PouchDB;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_class = default)]
     pub fn destroy(this: &PouchDB) -> Promise;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_class = default)]
     pub fn put(this: &PouchDB, doc: JsValue) -> Promise;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_class = default)]
     pub fn post(this: &PouchDB, doc: JsValue) -> Promise;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_class = default)]
     pub fn get(this: &PouchDB, docId: JsValue, options: JsValue) -> Promise;
 
-    #[wasm_bindgen(method)]
+    #[wasm_bindgen(method, js_class = default)]
     pub fn remove(this: &PouchDB, doc: JsValue) -> Promise;
 }
