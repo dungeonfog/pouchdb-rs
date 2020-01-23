2b1b13b9b5aaea6010bf25ab4c70adbffc8cf168
Andreas Monitzer <andreas@monitzer.com>
2020-01-23 20:59:33 +0100

    Our error type now implements std::error::Error.

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,6 @@
 use serde_json::Error as SerdeError;
 use wasm_bindgen::JsValue;
+use std::fmt::Debug;
 
 #[derive(Debug)]
 pub enum Error {
@@ -16,5 +17,23 @@ impl From<JsValue> for Error {
 impl From<SerdeError> for Error {
     fn from(err: SerdeError) -> Error {
         Error::Serde(err)
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Js(_) => None,
+            Self::Serde(err) => err.source(),
+        }
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Js(err) => err.fmt(f),
+            Self::Serde(err) => <SerdeError as std::fmt::Display>::fmt(err, f),
+        }
     }
 }
